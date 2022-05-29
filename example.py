48eafc3ca4e89de5db054b67c9eeac1a0662d34c
slow <lilian.filoche2007@gmail.com>
2022-05-29 14:05:42 +0200

    Delete example.py

--- example.py
+++
@@ -1,8 +0,0 @@
-from SmartnatorPy.smartnator import Smartnator
-
-if __name__ == "__main__":
-    sn = Smartnator()
-    email = sn.getEmail("dot")
-    print("Your email:", email)
-    message = sn.waitForMessage(email, sender_address="info@service-mail.zalando.fr")
-    print(message)
